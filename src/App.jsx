506af8262f74afd7439b1b204793ee3a2b8f546f
bhaveshsirvi <79744815+bhaveshsirvi@users.noreply.github.com>
2023-10-08 00:19:36 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Blockly React App</h1>
+        <h1>Logixblocks</h1>
       </header>
       <main>
         <BlocklyComponent />
