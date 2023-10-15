1d93616e790e41ce039bed27a430b443d22c7552
gauravmasalkar405 <125235613+gauravmasalkar405@users.noreply.github.com>
2023-10-15 19:52:20 +0530

    Yinitial project setup - redux and routing added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BlocklyComponent from "../components/BlocklyComponent";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>LogicBlocks</h1>
+      <BlocklyComponent />
+    </div>
+  );
+};
+
+export default Home;
